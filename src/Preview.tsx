12c97277d775bb6e6fa1ec8893a737a8aa1a85ce
Caesar1226 <1633490504@qq.com>
2025-01-08 16:14:34 +0800

    [feat] add test

--- src/Preview.tsx
+++ src/Preview.tsx
@@ -38,30 +38,74 @@ const annotationList = convertData(positions, n);
 function Preview() {
   const [curPositions, setCurPositions]: any = useState([]);
   const [origin_paper_size, setOriginPaperSize]: any = useState(null);
+  const [render_width, setrender_width]: any = useState(null);
   useEffect(() => {
     setTimeout(() => {
-      setOriginPaperSize({ width: 1191, height: 1684 });
+      setOriginPaperSize({ width: 1920, height: 1080 });
+      setrender_width(526);
     }, 500);
   }, []);
   const { Element, pdfRef, mdRef } = useFileViewer({
     fileUrl:
-      // 'http://10.15.12.13:9000/dev-rag-data/syt_test/syt_test_%E5%8F%B7%E6%96%87%E8%AF%86%E5%88%AB%E7%82%B9/V0/parsed/60%E5%8F%B7%E6%96%87%E8%AF%86%E5%88%AB%E7%82%B9.xlsx.md',
-      // 'http://10.15.12.13:9000/dev-rag-data/%E8%A1%A8%E6%A0%BC%E5%A4%9A%E8%A1%8C%E5%A4%9A%E5%88%97%E6%B5%8B%E8%AF%95/%E8%A1%A8%E6%A0%BC%E5%A4%9A%E8%A1%8C%E5%A4%9A%E5%88%97%E6%B5%8B%E8%AF%95_%E5%8F%B7%E6%96%87%E8%AF%86%E5%88%AB%E7%82%B9/V0/parsed/60%E5%8F%B7%E6%96%87%E8%AF%86%E5%88%AB%E7%82%B9.xlsx.md',
-      'http://10.15.12.13:9000/dev-rag-data/%E8%A1%A8%E6%A0%BC%E5%A4%9A%E8%A1%8C%E5%A4%9A%E5%88%97%E6%B5%8B%E8%AF%95/%E8%A1%A8%E6%A0%BC%E5%A4%9A%E8%A1%8C%E5%A4%9A%E5%88%97%E6%B5%8B%E8%AF%95_%E6%B6%89%E5%A4%96%E6%94%B6%E6%94%AF%E4%BB%A3%E7%A0%81%E8%A1%A8/V0/%E6%B6%89%E5%A4%96%E6%94%B6%E6%94%AF%E4%BB%A3%E7%A0%81%E8%A1%A8-1547451846112.pdf',
-    //'http://10.15.12.13:9000/dev-rag-data/syt_test/syt_test_%E8%BD%B4%E6%89%BF%E7%9F%A5%E8%AF%86%E8%BD%B4%E6%89%BF%E7%9A%84%E7%BB%93%E6%9E%84%E5%8F%8A%E5%90%84%E6%9E%84%E6%88%90%E9%9B%B6%E4%BB%B6%E7%9A%84%E4%BD%9C%E7%94%A8/V0/%E8%BD%B4%E6%89%BF%E7%9F%A5%E8%AF%86%EF%BC%883%EF%BC%89%E2%80%94%E2%80%94%E8%BD%B4%E6%89%BF%E7%9A%84%E7%BB%93%E6%9E%84%E5%8F%8A%E5%90%84%E6%9E%84%E6%88%90%E9%9B%B6%E4%BB%B6%E7%9A%84%E4%BD%9C%E7%94%A8.pdf',
+      'http://10.15.12.13:9000/dev-rag-data/qvq/qvq_%E7%A7%91%E6%8A%80%E9%87%91%E8%9E%8D%E7%9A%84%E5%85%AC%E5%8F%B8%E6%88%91%E4%BB%AC%E9%9C%80%E8%A6%81%E5%85%B3%E6%B3%A8%E5%93%AA%E4%BA%9B%E6%94%BF%E7%AD%96%E8%A1%8C%E4%B8%9A%E7%A0%94%E6%8A%A5/V0/%E2%80%9C%E7%A7%91%E6%8A%80+%E9%87%91%E8%9E%8D%E2%80%9D%E7%9A%84%E5%85%AC%E5%8F%B8%EF%BC%8C%E6%88%91%E4%BB%AC%E9%9C%80%E8%A6%81%E5%85%B3%E6%B3%A8%E5%93%AA%E4%BA%9B%E6%94%BF%E7%AD%96%EF%BC%9F2022-04-12%E8%A1%8C%E4%B8%9A%E7%A0%94%E6%8A%A5.pdf',
+    // 'http://10.15.12.13:9000/dev-rag-data/%E8%A1%A8%E6%A0%BC%E5%A4%9A%E8%A1%8C%E5%A4%9A%E5%88%97%E6%B5%8B%E8%AF%95/%E8%A1%A8%E6%A0%BC%E5%A4%9A%E8%A1%8C%E5%A4%9A%E5%88%97%E6%B5%8B%E8%AF%95_%E6%B6%89%E5%A4%96%E6%94%B6%E6%94%AF%E4%BB%A3%E7%A0%81%E8%A1%A8/V0/%E6%B6%89%E5%A4%96%E6%94%B6%E6%94%AF%E4%BB%A3%E7%A0%81%E8%A1%A8-1547451846112.pdf',
+    // 'http://10.15.12.13:9000/dev-rag-data/syt_test/syt_test_%E8%BD%B4%E6%89%BF%E7%9F%A5%E8%AF%86%E8%BD%B4%E6%89%BF%E7%9A%84%E7%BB%93%E6%9E%84%E5%8F%8A%E5%90%84%E6%9E%84%E6%88%90%E9%9B%B6%E4%BB%B6%E7%9A%84%E4%BD%9C%E7%94%A8/V0/%E8%BD%B4%E6%89%BF%E7%9F%A5%E8%AF%86%EF%BC%883%EF%BC%89%E2%80%94%E2%80%94%E8%BD%B4%E6%89%BF%E7%9A%84%E7%BB%93%E6%9E%84%E5%8F%8A%E5%90%84%E6%9E%84%E6%88%90%E9%9B%B6%E4%BB%B6%E7%9A%84%E4%BD%9C%E7%94%A8.pdf',
     // 'http://10.15.12.13:9000/dev-rag-data/%E5%AF%8C%E6%96%87%E6%9C%AC%E6%B5%8B%E8%AF%95test1/%E5%AF%8C%E6%96%87%E6%9C%AC%E6%B5%8B%E8%AF%95test1_%E8%BD%B4%E6%89%BF%E6%98%AF%E6%9C%BA%E6%A2%B0%E8%AE%BE%E5%A4%87%E4%B8%AD%E4%B8%BE%E8%B6%B3%E8%BD%BB%E9%87%8D%E7%9A%84%E9%9B%B6%E9%83%A8%E4%BB%B6%E5%89%AF%E6%9C%AC/V0/%E8%BD%B4%E6%89%BF%E6%98%AF%E6%9C%BA%E6%A2%B0%E8%AE%BE%E5%A4%87%E4%B8%AD%E4%B8%BE%E8%B6%B3%E8%BD%BB%E9%87%8D%E7%9A%84%E9%9B%B6%E9%83%A8%E4%BB%B6-%E5%89%AF%E6%9C%AC.pdf',
     //'http://10.15.12.13:9000/dev-rag-data/%E6%99%AE%E9%80%9A%E6%96%87%E4%BB%B6%E6%B5%8B%E8%AF%95/%E6%99%AE%E9%80%9A%E6%96%87%E4%BB%B6%E6%B5%8B%E8%AF%95_n%E6%A1%88IWT%E4%BA%94%E5%BA%93%E6%95%B0%E6%8D%AE%E6%A0%87%E5%87%86%E6%A8%A1%E7%89%88%E6%A1%88%E4%BE%8B%E5%BA%93/V0/n%E6%A1%88IWT_%E4%BA%94%E5%BA%93%E6%95%B0%E6%8D%AE%E6%A0%87%E5%87%86%E6%A8%A1%E7%89%88-%E6%A1%88%E4%BE%8B%E5%BA%93.xlsx',
     form: 'pdf',
     annotation: {
       method: 'position',
-      data: curPositions,
-      origin_paper_size,
+      data: [
+        {
+          id: 'f0f10904c5c',
+          page: 1,
+          anotation_color: 'rgba(223,231,255,.5)',
+          position: [
+            [295, 438],
+            [1624, 438],
+            [1624, 505],
+            [295, 505],
+          ],
+        },
+        {
+          id: '0f10904c5c9',
+          page: 1,
+          anotation_color: 'rgba(223,231,255,.5)',
+          position: [
+            [67, 844],
+            [791, 844],
+            [791, 873],
+            [67, 873],
+          ],
+        },
+        {
+          id: 'f10904c5c92',
+          page: 1,
+          anotation_color: 'rgba(223,231,255,.5)',
+          position: [
+            [67, 878],
+            [911, 878],
+            [911, 911],
+            [67, 911],
+          ],
+        },
+        {
+          id: '10904c5c92c',
+          page: 1,
+          anotation_color: 'rgba(223,231,255,.5)',
+          position: [
+            [67, 914],
+            [934, 914],
+            [934, 947],
+            [67, 947],
+          ],
+        },
+      ],
+      origin_paper_size: { width: 1920, height: 1080 },
     },
-    render_width: 660,
+    bgColor: '#fff',
+    render_width: 526,
     hide_toolbar: true,
-    actionOnEmmit: (type: string) => {
-      console.log('type', type);
-    },
   });
 
   // useEffect(() => {
@@ -89,7 +133,52 @@ function Preview() {
       <button
         onClick={() => {
           pdfRef.current?.pageChange(1);
-          setCurPositions(annotationList);
+          setCurPositions([
+            {
+              id: 'a33e1eb8010',
+              page: 1,
+              anotation_color: 'rgba(223,231,255,.5)',
+              position: [
+                [295, 438],
+                [1624, 438],
+                [1624, 505],
+                [295, 505],
+              ],
+            },
+            {
+              id: '33e1eb8010a',
+              page: 1,
+              anotation_color: 'rgba(223,231,255,.5)',
+              position: [
+                [67, 844],
+                [791, 844],
+                [791, 873],
+                [67, 873],
+              ],
+            },
+            {
+              id: '3e1eb8010a2',
+              page: 1,
+              anotation_color: 'rgba(223,231,255,.5)',
+              position: [
+                [67, 878],
+                [911, 878],
+                [911, 911],
+                [67, 911],
+              ],
+            },
+            {
+              id: 'e1eb8010a21',
+              page: 1,
+              anotation_color: 'rgba(223,231,255,.5)',
+              position: [
+                [67, 914],
+                [934, 914],
+                [934, 947],
+                [67, 947],
+              ],
+            },
+          ]);
         }}
       >
         翻页1
